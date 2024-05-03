7b1908fef59b98a28a99ee8f7e8fc50fe94adba5
Adilet <adiletkanybekovic03@gmail.com>
2024-05-03 21:38:55 +0200

    init:randometer

--- cart/ContentView.swift
+++ cart/ContentView.swift
@@ -9,16 +9,107 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Image("Background")
+            
+            VStack{
+                Spacer()
+                Spacer()
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    
+                    Image("dice1")
+                        .padding()
+                    
+                    Image("dice2")
+                        .padding()
+                    
+                }
+                Spacer()
+                Button(action: {
+                            // Обработка нажатия кнопки "Roll"
+                        }) {
+                            Text("Start")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                Spacer()
+                HStack{
+                    
+                
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
+
     }
 }
 
 #Preview {
     ContentView()
 }
+/*
+ ZStack{
+     Color(.red)
+         .ignoresSafeArea()
+     
+     VStack(alignment: .leading, spacing: 20.0) {
+         Image("image1")
+             .resizable()
+             .aspectRatio(contentMode: .fit)
+             .cornerRadius(15)
+         
+         HStack {
+             
+             Spacer()
+             Text("Bishkek")
+                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+             Spacer()
+             
+             VStack{
+                 
+                 HStack{
+                     Image(systemName: "star.fill")
+                         .foregroundColor(.orange)
+                     Image(systemName: "star.fill")
+                         .foregroundColor(.orange)
+                     Image(systemName: "star.fill")
+                         .foregroundColor(.orange)
+                     Image(systemName: "star.fill")
+                         .foregroundColor(.orange)
+                     Image(systemName: "star.fill")
+                         .foregroundColor(.orange)
+                     
+                     
+                     
+                 }
+                 Text("Rewiews 983")
+                 }
+             .foregroundColor(.orange)
+             .font(.caption2)
+         }
+         
+         
+         Text("Come visit Kygyz mountains for an experience of a lifetime.")
+         
+         HStack{
+             Spacer()
+             Image(systemName: "fork.knife")
+             Image(systemName: "binoculars.fill")
+             
+         }
+     }
+     .padding()
+     .background(Rectangle().foregroundColor(.white).cornerRadius(15) .shadow(radius: 15))
+     .padding()
+             
+     
+ }
+ */
